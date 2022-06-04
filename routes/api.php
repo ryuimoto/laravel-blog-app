cf719386c9fa326ca487e59dab5014436d45d354
maca <narrow617@gmail.com>
2022-06-04 17:12:48 +0900

    add any method

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -31,4 +32,21 @@ Route::group(['middleware' => ['auth:sanctum']],function(){
     // User
     Route::get('/user',[AuthController::class,'user']);
     Route::post('/logout',[AuthController::class,'logout']);
+
+    // Post
+    Route::get('/posts',[PostController::class,'index']); // all posts
+    Route::post('/posts',[PostController::class,'store']); // create post
+    Route::get('/posts/{id}',[PostController::class,'show']); // get signal post
+    Route::put('/posts/{id}',[PostController::class,'update']); // update post
+    Route::delete('/posts/{id}',[PostController::class,'destroy']); // delete post
+
+    // Comment
+    Route::get('/posts/{id}/comments',[CommentController::class,'index']); // all comments of a post
+    Route::post('/posts/{id}/comments',[CommentController::class,'store']); // create comment on a post
+    Route::put('/comments/{id}',[CommentController::class,'update']); // update a comment
+    Route::delete('/comments/{id}',[CommentController::class,'destroy']); // delete a comment
+
+    // Like
+    Route::post('/posts/{id}/likes',[LikeController::class,'likeOrUnlike']); // like or dislike back a post
+    
 });
